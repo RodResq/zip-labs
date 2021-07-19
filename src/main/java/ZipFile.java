c81d32144f0c1ade3f486cdea98a0a3300b0e2f4
RodResq <rodrigoresque@yahoo.com.br>
2021-07-19 00:47:39 -0300

    refatoracao estrutura do projeto

--- src/main/java/ZipFile.java
+++ src/main/java/ZipFile.java
@@ -1,30 +1,13 @@
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
 import java.io.IOException;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipOutputStream;
+
+import br.com.local.ziphelper.ZipSingleFile;
 
 public class ZipFile {
 
 	public static void main(String[] args) throws IOException {
 		
-		String sourceFile = "src/main/resources/test1.txt";
-		FileOutputStream fos = new FileOutputStream("src/main/resources/compressed.zip");
-		ZipOutputStream zipOut = new ZipOutputStream(fos);
-		File fileToZip = new File(sourceFile);
-		FileInputStream fis = new FileInputStream(fileToZip);
-		ZipEntry zipEntry = new ZipEntry(fileToZip.getName());
-		zipOut.putNextEntry(zipEntry);
-		byte[] bytes  = new byte[1024];
-		int length;
-		while ((length = fis.read(bytes)) >= 0) {
-			zipOut.write(bytes, 0, length);
-			
-		}
-		zipOut.close();
-		fis.close();
-		fos.close();
+		ZipSingleFile zipSingle = new ZipSingleFile();
+		zipSingle.createZip();
 	}
 
 }
