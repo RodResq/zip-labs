ef9ef70e7d8e54116df142c71f9d97ad16a3be6f
RodResq <rodrigoresque@yahoo.com.br>
2021-07-19 01:11:11 -0300

    criacao classe zipMultiplesFiles

--- src/main/java/ZipFile.java
+++ src/main/java/ZipFile.java
@@ -1,13 +1,17 @@
 import java.io.IOException;
 
+import br.com.local.ziphelper.ZipMultipleFiles;
 import br.com.local.ziphelper.ZipSingleFile;
 
 public class ZipFile {
 
 	public static void main(String[] args) throws IOException {
 		
-		ZipSingleFile zipSingle = new ZipSingleFile();
-		zipSingle.createZip();
+//		ZipSingleFile zipSingle = new ZipSingleFile();
+//		zipSingle.createZip();
+		
+		ZipMultipleFiles zipMultipleFiles = new ZipMultipleFiles();
+		zipMultipleFiles.createMultiplesZipFiles();
 	}
 
 }
