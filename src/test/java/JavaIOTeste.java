b32a0cdbfefacd81c2c215d148a6d2c13a1f4b7d
RodResq <rodrigoresque@yahoo.com.br>
2023-06-03 23:23:15 -0300

    Explores Java.IO

--- src/test/java/JavaIOTeste.java
+++ src/test/java/JavaIOTeste.java
@@ -1,12 +1,18 @@
+import org.apache.commons.io.FileUtils;
 import org.junit.Test;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+
+import static org.junit.Assert.assertFalse;
+
 
 public class JavaIOTeste {
 
@@ -20,8 +26,37 @@ public class JavaIOTeste {
     }
 
     @Test
-    public void testCresateFile() throws IOException {
+    public void testCriandoFileComPath() throws IOException {
         Path newFilePath = Paths.get(FILE_NAME);
         Files.createFile(newFilePath);
+    }
+
+    @Test
+    public void testCriandoFileComFile() throws IOException {
+        File newFile = new File(FILE_NAME);
+        boolean success = newFile.createNewFile();
+
+        assertFalse(success);
+    }
+
+    @Test
+    public void testCriandoFileComFileOutputStream() {
+        try(FileOutputStream fileOutputStream = new FileOutputStream(FILE_NAME)) {
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void testCriandoArquivoComGuava() throws IOException {
+        com.google.common.io.Files.touch(new File(FILE_NAME));
+    }
+
+    @Test
+    public void testCriandoFileComApacheCommonsIO() throws IOException {
+        FileUtils.touch(new File(FILE_NAME));
     }
 }
